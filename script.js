247772319c8b506c3d622105d7b4f5722e6379ad
Michael Darko <mykdarkoxyz@gmail.com>
2025-06-17 17:48:59 +0000

    added function()

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById('form-feedback');
   
 
-    form.addEventListener('submit',(event)=>{
+    form.addEventListener('submit',function(event){
         event.preventDefault();
         const username = document.getElementById('username').value.trim();
         const email = document.getElementById('email').value.trim();
